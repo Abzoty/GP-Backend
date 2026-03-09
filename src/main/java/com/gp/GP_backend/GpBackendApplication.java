99824918126cf930fc87c92936ab37cd303a79ad
Yosef <yosefkarem68@gmail.com>
2026-03-09 21:39:33 +0200

    fix modelmapper Bean issue

--- src/main/java/com/gp/GP_backend/GpBackendApplication.java
+++ src/main/java/com/gp/GP_backend/GpBackendApplication.java
@@ -1,7 +1,9 @@
 package com.gp.GP_backend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
@@ -11,6 +13,11 @@ public class GpBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GpBackendApplication.class, args);
 	}
+
+	@Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
 }
 
